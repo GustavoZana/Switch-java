fd2c4058f12da631fc20fe48815c1296f8a32e93
zana <zana@DESKTOP-RSGLRDR>
2026-09-11 20:22:47 -0300

    exercicios

---
+++ Switchaula/src/switch_1/exercicios_1.java
@@ -0,0 +1,31 @@
+package switch_1;
+
+ import java.util.Scanner;
+
+public class exercicios_1 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro numero:");
+		
+		double num1 = entrada.nextDouble();
+		
+		System.out.print("Digite o segundo numero");
+		
+		double num2 = entrada.nextDouble();
+		
+		System.out.print("Digite a operacao(+, - , * , /):");
+		
+		char operador = entrada.next().charAt(0);
+		 
+		switch (operador) {
+		
+		
+		}
+		
+	}
+
+}
---
+++ Switchaula/src/switch_1/switch_exemplo_2.java
@@ -0,0 +1,10 @@
+package switch_1;
+
+public class switch_exemplo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
